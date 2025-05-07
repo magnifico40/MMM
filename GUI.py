5f675a27ad8cb905e97022a7919967083d2d1e82
Filip Pawelec <fpawelec0@gmail.com>
2025-05-07 23:46:08 +0200

    gui in class, iterations slider

--- GUI.py
+++ GUI.py
@@ -6,6 +6,11 @@ from matplotlib.backends.backend_wxagg import NavigationToolbar2WxAgg as Navigat
 from matplotlib.figure import Figure
 
 
+import numpy as np
+import scipy
+import matplotlib.pyplot as plt
+from main2 import Simulation
+
 class ChartPanel(wx.Panel):
     def __init__(self, parent):
         super().__init__(parent)
@@ -18,9 +23,12 @@ class ChartPanel(wx.Panel):
 
         self.SetSizer(sizer)
 
+
 class ChartFrame(wx.Frame):
     def __init__(self):
-        super().__init__(None, title="Simulation", size=(800, 600))
+        super().__init__(None, title="Simulation", size=(1440, 1000))
+
+        self.chartData = Simulation()
 
         self.splitter = wx.SplitterWindow(self)
 
@@ -37,7 +45,7 @@ class ChartFrame(wx.Frame):
             majorDimension=1,
             style=wx.RA_SPECIFY_COLS
         )
-        self.chart_radio.Bind(wx.EVT_RADIOBOX, self.on_input_signal_change) # event
+        self.chart_radio.Bind(wx.EVT_RADIOBOX, self.on_input_signal_change)  # event
         chart_sizer.Add(self.chart_radio, 0, wx.EXPAND | wx.ALL, 5)
         control_sizer.Add(chart_sizer, 0, wx.EXPAND | wx.ALL, 10)
 
@@ -47,28 +55,35 @@ class ChartFrame(wx.Frame):
 
         self.iter_slider = wx.Slider(
             self.control_panel,
-            minValue=1,
-            maxValue=100000,
+            value=1000,
+            minValue=100,
+            maxValue=8000,
             style=wx.SL_HORIZONTAL | wx.SL_LABELS
         )
-        self.iter_slider.Bind(wx.EVT_SLIDER, self.on_iter_change) # slider event
+        self.iter_slider.Bind(wx.EVT_SLIDER, self.on_iter_change)  # slider event
         iter_sizer.Add(self.iter_slider, 0, wx.EXPAND | wx.ALL, 5)
         control_sizer.Add(iter_sizer, 0, wx.EXPAND | wx.ALL, 10)
 
         # Button
         self.random_btn = wx.Button(self.control_panel, label="Random settings")
-        self.random_btn.Bind(wx.EVT_BUTTON, self.on_random_settings) # random btn event
+        self.random_btn.Bind(wx.EVT_BUTTON, self.on_random_settings)  # random btn event
         control_sizer.Add(self.random_btn, 0, wx.EXPAND | wx.ALL, 10)
-
 
         self.control_panel.SetSizer(control_sizer)
 
+        self.v_splitter = wx.SplitterWindow(self.splitter)
         # Matplot panel
-        self.plot_panel = ChartPanel(self.splitter)
+        self.plot1_panel = ChartPanel(self.v_splitter)
+        self.plot2_panel = ChartPanel(self.v_splitter)
 
-        self.splitter.SplitVertically(self.control_panel, self.plot_panel)
+        self.splitter.SplitVertically(self.control_panel, self.v_splitter)
         self.splitter.SetMinimumPaneSize(200)
-        self.splitter.SetSashPosition(250)
+        self.splitter.SetSashPosition(350)
+
+        self.v_splitter.SplitHorizontally(self.plot1_panel, self.plot2_panel)
+        self.v_splitter.SetMinimumPaneSize(100)
+        self.v_splitter.SetSashPosition(self.GetSize().GetHeight() // 2)
+        self.Bind(wx.EVT_SIZE, self.OnSize)
 
         frame_sizer = wx.BoxSizer(wx.VERTICAL)
         frame_sizer.Add(self.splitter, 1, wx.EXPAND)
@@ -78,15 +93,52 @@ class ChartFrame(wx.Frame):
 
         self.Centre()
 
-    def update_chart(self):
-        self.plot_panel.figure.clear()
+    def OnSize(self, event):
+        # When the window is resized, keep the panels equal
+        size = self.GetSize()
+        self.v_splitter.SetSashPosition(size.GetHeight() // 2)
+        event.Skip()
 
-        ax = self.plot_panel.figure.add_subplot(111)
-        ax.plot(self.data_x, self.data_y, color='blue')
+    def update_chart(self):
+        self.plot1_panel.figure.clear()
+        self.plot2_panel.figure.clear()
+
+        dataX1, dataX2, dataT = self.chartData.getRK4ChartData()
+        ax = self.plot1_panel.figure.add_subplot(111)
+        ax.plot(dataT, dataX1, color='blue')
+        ax.plot(dataT, dataX2, color='orange')
 
         ax.grid(True)
         ax.set_xlabel('X axis')
         ax.set_ylabel('Y axis')
         ax.set_title('RK4 Chart')
 
-        self.plot_panel.canvas.draw()
+        EdataX1, EdataX2, EdataT = self.chartData.getEulerChartData()
+        ax = self.plot2_panel.figure.add_subplot(111)
+        ax.plot(EdataT, EdataX1, color='red')
+        ax.plot(EdataT, EdataX2, color='green')
+
+        ax.grid(True)
+        ax.set_xlabel('X axis')
+        ax.set_ylabel('Y axis')
+        ax.set_title('Euler Chart')
+
+        self.plot1_panel.canvas.draw()
+        self.plot2_panel.canvas.draw()
+
+    def on_input_signal_change(self):
+        pass
+
+    def on_iter_change(self, event):
+        self.chartData.setIterationsNumber(self.iter_slider.GetValue())
+        self.update_chart()
+
+    def on_random_settings(self):
+        pass
+
+
+if __name__ == '__main__':
+    app = wx.App()
+    frame = ChartFrame()
+    frame.Show()
+    app.MainLoop()
--- main2.py
+++ main2.py
@@ -2,6 +2,7 @@ import numpy as np
 import scipy
 import matplotlib.pyplot as plt
 
+
 class Simulation:
     def __init__(self):
         # parameters:
@@ -33,37 +34,37 @@ class Simulation:
         self.function = 'sin'
         self.duty_cycle = 0.5
 
-    def squareSignal(self, readTime):
+    def __squareSignal(self, readTime):
         value = self.amplitude * scipy.signal.square(2 * np.pi * self.frequency * readTime, duty=self.dutyCycle)
         return value
 
-    def sawToothSignal(self, readTime):
+    def __sawToothSignal(self, readTime):
         value = self.amplitude * scipy.signal.sawtooth(2 * np.pi * self.frequency * readTime)
         return value
 
-    def sinSignal(self, readTime):
+    def __sinSignal(self, readTime):
         value = self.amplitude * np.sin(2 * np.pi * self.frequency * readTime)
         return value
 
-    def f(self, x1, x2, t):
+    def __f(self, x1, x2, t):
         a = (-self.b) / (self.J2 + self.n ** 2 * self.J1)
         b = (-self.k) / (self.J2 + self.n ** 2 * self.J1)
         c = (1/self.n) / (self.J2 + self.n ** 2 * self.J1)
 
         if self.function == 'square':
-            Tm = self.squareSignal(t)
+            Tm = self.__squareSignal(t)
         elif self.function == 'sawTooth':
-            Tm = self.sawToothSignal(t)
+            Tm = self.__sawToothSignal(t)
         elif self.function == 'sin':
-            Tm = self.sinSignal(t)
+            Tm = self.__sinSignal(t)
         else: Tm = 0
 
         return a * x1 + b * x2 + c * Tm
 
-    def g(self, x1, x2, t):
+    def __g(self, x1, x2, t):
         return x2
 
-    def RK4(self):
+    def __RK4(self):
         self.RKx1Values.clear()
         self.RKx2Values.clear()
         self.tValues.clear()
@@ -75,23 +76,23 @@ class Simulation:
             self.RKx2Values.append(x2_0)
             self.tValues.append(t0)
 
-            k1 = self.h * self.g(x1_0, x2_0, t0)
-            l1 = self.h * self.f(x1_0, x2_0, t0)
+            k1 = self.h * self.__g(x1_0, x2_0, t0)
+            l1 = self.h * self.__f(x1_0, x2_0, t0)
 
-            k2 = self.h * self.g(x1_0 + self.h / 2, x2_0 + k1 / 2, t0 + l1 / 2)
-            l2 = self.h * self.f(x1_0 + self.h / 2, x2_0 + k1 / 2, t0 + l1 / 2)
+            k2 = self.h * self.__g(x1_0 + self.h / 2, x2_0 + k1 / 2, t0 + l1 / 2)
+            l2 = self.h * self.__f(x1_0 + self.h / 2, x2_0 + k1 / 2, t0 + l1 / 2)
 
-            k3 = self.h * self.g(x1_0 + self.h / 2, x2_0 + k2 / 2, t0 + l2 / 2)
-            l3 = self.h * self.f(x1_0 + self.h / 2, x2_0 + k2 / 2, t0 + l2 / 2)
+            k3 = self.h * self.__g(x1_0 + self.h / 2, x2_0 + k2 / 2, t0 + l2 / 2)
+            l3 = self.h * self.__f(x1_0 + self.h / 2, x2_0 + k2 / 2, t0 + l2 / 2)
 
-            k4 = self.h * self.g(x1_0 + self.h, x2_0 + k3, t0 + l3)
-            l4 = self.h * self.f(x1_0 + self.h, x2_0 + k3, t0 + l3)
+            k4 = self.h * self.__g(x1_0 + self.h, x2_0 + k3, t0 + l3)
+            l4 = self.h * self.__f(x1_0 + self.h, x2_0 + k3, t0 + l3)
 
             x1_0 = x1_0 + (k1 + 2*k2 + 2*k3 + k4) / 6
             x2_0 = x2_0 + (l1 + 2*l2 + 2*l3 + l4) / 6
             t0 += self.h
 
-    def Euler(self):
+    def __Euler(self):
         self.Ex1Values.clear()
         self.Ex2Values.clear()
         self.tValues.clear()
@@ -104,16 +105,21 @@ class Simulation:
             self.Ex2Values.append(x2_0)
             self.tValues.append(t0)
 
-            x1_0 = x1_0 + self.h * self.g(x1_0, x2_0, t0)
-            x2_0 = x2_0 + self.h * self.f(x1_0, x2_0, t0)
+            x1_0 = x1_0 + self.h * self.__g(x1_0, x2_0, t0)
+            x2_0 = x2_0 + self.h * self.__f(x1_0, x2_0, t0)
 
     def getRK4ChartData(self):
+        self.__RK4()
         return self.RKx1Values, self.RKx2Values, self.tValues
 
     def getEulerChartData(self):
+        self.__Euler()
         return self.Ex1Values, self.Ex2Values, self.tValues
 
+    def setIterationsNumber(self, num):
+        self.N = num
 
+'''
 a = Simulation()
 a.RK4()
 a.Euler()
@@ -158,4 +164,4 @@ plt.title("Trajektoria (Euler)")
 plt.legend()
 plt.grid()
 plt.show()
-"""
+'''
